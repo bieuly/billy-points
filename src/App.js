f1dee7b0d5bb350943665beaea48d43af3ea7a88
Billy Hoang <billy-hoang@hotmail.com>
2018-09-24 23:45:47 -0700

    feat: connect to firebase and get data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      points: 0
+    }
+  }
+
+  componentDidMount() {
+    const pointsRef = firebase.database().ref().child("points");
+    const billyRef = pointsRef.child("billy");
+    const billyPointsRef = billyRef.child("points");
+    billyPointsRef.on('value', snap => {
+      this.setState({
+        points: snap.val()
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        hello billy
+        {`You have ${this.state.points} billy points!`}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
 
+var config = {
+        apiKey: "AIzaSyA1GbbL5AGnvEjmt85H437wTUQ9FEhLKp8",
+        authDomain: "billy-points.firebaseapp.com",
+        databaseURL: "https://billy-points.firebaseio.com",
+        projectId: "billy-points",
+        storageBucket: "billy-points.appspot.com",
+        messagingSenderId: "285780503457"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

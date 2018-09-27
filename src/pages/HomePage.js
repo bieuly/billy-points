9f07faa011fa8871b06a7535f946323953376222
Billy Hoang <billy-hoang@hotmail.com>
2018-09-26 20:45:54 -0700

    style: style homepage with css grid and semantic ui

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,46 +1,101 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { Button, Card, Icon, Form, Image, Segment } from 'semantic-ui-react'
 import styled from 'styled-components';
+import firebase from 'firebase';
 
 const HomePageContainer = styled.div`
     height: 100%;
+    display: grid;
+    grid-template-areas:
+        ". . . .  ."
+        ". . title loginForm ."
+        ". . . . ."
+`;
+
+const LoginFormContainer = styled.div`
+    grid-area: loginForm
+`;
+
+const LoginForm = styled(Form)`
+    height: 100%;
+`;
+
+const FormButton = styled(Button)`
+    margin-bottom: 10px !important;
+`;
+
+const TitleContainer = styled.div`
+    grid-area: title;
+    max-width: 300px;
 `;
 
 class HomePage extends Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            login: {
+                username: "",
+                password: ""
+            }
+        }
+    }
+
+
+
+    handleOnChange = (e) => {
+        this.setState({
+            login: {
+                ...this.state.login,
+                [e.target.id]: e.target.value
+            }
+        })
+    }
+
+    handleLoginSubmit = async () => {
+        await firebase.auth().signInWithEmailAndPassword(this.state.login.username, this.state.login.password);
+        firebase.auth().onAuthStateChanged((user) => {
+            this.props.history.push("/dashboard")
+        })
     }
 
     render() {
         return (
             <HomePageContainer>
-                <Grid textAlign='' style={{ height: '100%' }} verticalAlign='middle'>
-                <Grid.Column style={{ maxWidth: 450 }}>
-                    <Form size='large'>
-                    <Segment stacked>
-                        <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-                        <Form.Input
-                        fluid
-                        icon='lock'
-                        iconPosition='left'
-                        placeholder='Password'
-                        type='password'
-                        />
-
-                        <Link to="/dashboard">
-                            <Button primary fluid size='large'>
-                            Login
-                            </Button>
-                        </Link>
-                    </Segment>
-                    </Form>
-                    <Message>
-                    New to us? <a href='#'>Sign Up</a>
-                    </Message>
-                </Grid.Column>
-                </Grid>
+                <TitleContainer>
+                    <h1>Hello!</h1>
+                    <h2>Register to keep track of your points</h2>
+                </TitleContainer>
+                <LoginFormContainer>
+                    <h1></h1>
+                    <LoginForm onSubmit={this.handleLoginSubmit}>
+                            <Segment>
+                                <Form.Input
+                                    id='username'
+                                    fluid
+                                    icon='user'
+                                    iconPosition='left'
+                                    placeholder='E-mail address'
+                                    onChange={(e)=>this.handleOnChange(e)}
+                                />
+                                <Form.Input
+                                    id='password'
+                                    fluid
+                                    icon='lock'
+                                    iconPosition='left'
+                                    placeholder='Password'
+                                    type='password'
+                                    onChange={(e)=>this.handleOnChange(e)}
+                                />
+                                <FormButton id="login" type="submit" primary fluid size='large'>
+                                Login
+                                </FormButton>
+                                <FormButton id="signup" type="submit" fluid size='large'>
+                                Sign Up
+                                </FormButton>
+                            </Segment>
+                        </LoginForm>
+                </LoginFormContainer>
             </HomePageContainer>
         )
     }
